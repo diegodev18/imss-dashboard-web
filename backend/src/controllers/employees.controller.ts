6e0282cb93c9717f8f3af444f63aafafffc6300b
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-11-07 17:24:21 -0600

    feat: add RFC format validation to employee creation process

--- backend/src/controllers/employees.controller.ts
+++ backend/src/controllers/employees.controller.ts
@@ -1,8 +1,11 @@
+import type { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
 import { Request, Response } from "express";
 
 import type { AddEmployeeReq, SessionRequest } from "@/types";
 
 import { prisma } from "@/lib/prisma";
+import { rfcValidator } from "@/utils/validator";
 
 export const addEmployee = async (req: SessionRequest, res: Response) => {
   if (!req.session?.user) {
@@ -27,18 +30,36 @@ export const addEmployee = async (req: SessionRequest, res: Response) => {
 
   const { curp, fullName, position, rfc, salary } = body;
 
-  await prisma.employees.create({
-    data: {
-      created_by: req.session.user.id,
-      curp,
-      full_name: fullName,
-      position,
-      rfc,
-      salary,
-    },
-  });
+  if (!rfcValidator(rfc)) {
+    return res.status(400).json({ message: "Invalid RFC format." });
+  }
 
-  res.status(200).json({ message: "Employee added successfully." });
+  try {
+    await prisma.employees.create({
+      data: {
+        created_by: req.session.user.id,
+        curp,
+        full_name: fullName,
+        position,
+        rfc,
+        salary,
+      },
+    });
+
+    res.status(200).json({ message: "Employee added successfully." });
+  } catch (error) {
+    const prismaError = error as PrismaClientKnownRequestError;
+
+    if (prismaError.code === "P2002") {
+      return res
+        .status(400)
+        .json({ message: "Employee with this CURP or RFC already exists." });
+    }
+
+    console.error("Error adding employee:", error);
+
+    res.status(500).json({ message: "Internal server error." });
+  }
 };
 
 export const getEmployees = async (req: SessionRequest, res: Response) => {
