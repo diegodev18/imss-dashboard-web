9fc212b66d351a04f696d04b16a99467ad80d5d3
Diego Sanchez <diegosanchez.dev018@gmail.com>
2025-11-07 16:46:12 -0600

    feat: add employee controller with basic CRUD function stubs

---
+++ backend/src/controllers/employees.controller.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const addEmployee = (req: Request, res: Response) => {
+  // Implementation for adding an employee
+};
+
+export const getEmployees = (req: Request, res: Response) => {
+  // Implementation for retrieving employees
+};
+
+export const updateEmployee = (req: Request, res: Response) => {
+  // Implementation for updating an employee
+};
